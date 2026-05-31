bbd22d5ff301745cc6f9fde3e7c81195cd90465e
slizhunter <daniel.frazier@gmail.com>
2026-05-31 13:22:03 -0400

    refactored auth

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/alexedwards/argon2id"
@@ -8,12 +9,26 @@ import (
 	"github.com/google/uuid"
 )
 
+type TokenType string
+
+const (
+	TokenTypeAccess TokenType = "chirpy-access"
+)
+
 func HashPassword(password string) (string, error) {
-	return argon2id.CreateHash(password, argon2id.DefaultParams)
+	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
+	if err != nil {
+		return "", err
+	}
+	return hash, nil
 }
 
 func CheckPasswordHash(password, hash string) (bool, error) {
-	return argon2id.ComparePasswordAndHash(password, hash)
+	match, err := argon2id.ComparePasswordAndHash(password, hash)
+	if err != nil {
+		return false, err
+	}
+	return match, nil
 }
 
 // MakeJWT generates a JWT for the given user ID with the specified secret and expiration duration.
@@ -28,9 +43,9 @@ func CheckPasswordHash(password, hash string) (bool, error) {
 //	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", nil
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
-		Issuer:    "chirpy-access",
-		IssuedAt:  jwt.NewNumericDate(time.Now()),
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Issuer:    string(TokenTypeAccess),
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
 		Subject:   userID.String(),
 	})
 	return token.SignedString([]byte(tokenSecret))
@@ -43,9 +58,21 @@ func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
 	if err != nil {
 		return uuid.Nil, err
 	}
-	id, err := token.Claims.GetSubject()
-	if err != nil || !token.Valid {
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil {
 		return uuid.Nil, err
 	}
-	return uuid.MustParse(id), nil
+
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, jwt.ErrTokenInvalidIssuer
+	}
+	id, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, fmt.Errorf("invalid user ID: %w", err)
+	}
+	return id, nil
 }
