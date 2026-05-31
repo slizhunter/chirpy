04ce51292d3604f1d0cd7cdfa7affac993ff99bf
slizhunter <daniel.frazier@gmail.com>
2026-05-30 20:18:04 -0400

    feat(auth) implement JWT and validation

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"time"
+
 	"github.com/alexedwards/argon2id"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func HashPassword(password string) (string, error) {
@@ -10,4 +14,38 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) (bool, error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
+}
+
+// MakeJWT generates a JWT for the given user ID with the specified secret and expiration duration.
+//
+// example:
+//
+//	token, err := MakeJWT(userID, "secret", time.Hour)
+//
+// returns the signed JWT token string and any error encountered.
+// example return:
+//
+//	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...", nil
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	})
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.Nil, err
+	}
+	id, err := token.Claims.GetSubject()
+	if err != nil || !token.Valid {
+		return uuid.Nil, err
+	}
+	return uuid.MustParse(id), nil
 }
